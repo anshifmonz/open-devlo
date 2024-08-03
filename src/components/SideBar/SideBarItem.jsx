aae3bee8ea7a383004a00477fd8ba8845c362deb
Muhammed Anshif <muhammedanshif236@gmail.com>
2024-08-03 12:03:08 +0530

    sideBar imroved

--- src/components/SideBar/SideBarItem.jsx
+++ src/components/SideBar/SideBarItem.jsx
@@ -1,24 +1,30 @@
 import { useState } from 'react';
 
-
-function SideBarItem({item}) {
-
-    const [isOpen, setOpen] = useState(false)
+function SideBarItem({ item }) {
+    const [isOpen, setOpen] = useState(false);
 
     return (
-        <div className={ isOpen ? "side-bar-item open" : "side-bar-item" }>
-            <div className="side-bar-title" onClick={() => { setOpen(!isOpen) }}>
-                { item.path ? <a href={item.path}>{item.title}</a> : <p>{item.title}</p> }
-                { item.childrens && <img src="assets/dropdown.png" alt="dropdown icons"/>}
+        <div className={isOpen ? "side-bar-item open" : "side-bar-item"}>
+            <div className="side-bar-title" onClick={() => setOpen(!isOpen)}>
+                {item.path ? <a href={item.path}>{item.title}</a> : <p>{item.title}</p>}
+                {item.childrens && (
+                    <img 
+                        src="assets/dropdown.png" 
+                        alt="dropdown icon" 
+                        className={isOpen ? "dropdown open" : ""} 
+                    />
+                )}
             </div>
 
-            {item.childrens && 
+            { item.childrens && 
                 <div className='side-bar-content'>
-                    { item.childrens.map((child, index) => <a href={child.path} key={index} className="side-bar-item open">{child.title}</a>)}
+                    {item.childrens.map((child, index) => (
+                        <SideBarItem item={child} key={index} />
+                    ))}
                 </div>
             }
         </div>
-    )
+    );
 }
 
 export default SideBarItem;
