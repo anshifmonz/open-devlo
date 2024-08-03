9dfe041e1eda9cdc5a5e16589b9ac88e40440136
Muhammed Anshif <muhammedanshif236@gmail.com>
2024-08-03 07:16:31 +0530

    make a small change in App.jsx before creating a new branch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,12 @@
 import '../styles/app.css'
 import NavBar from "./NavBar";
 import SideBar from "./SideBar/SideBar" 
-import Main from "./Main";
-import Home from "./Home";
 
 function App() {
     return (
         <>
             <NavBar />
-            <Home />
-            {/* <div id="main">
-                <SideBar />
-                <Main />
-            </div> */}
+            <SideBar />
         </>
     );
 }
