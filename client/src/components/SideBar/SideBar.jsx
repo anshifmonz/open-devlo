0caa1573902bbd8429c2e20f118721a2ed0a69f6
Muhammed Anshif <muhammedanshif236@gmail.com>
2024-09-19 14:02:54 +0530

    feat(sidebar): implement dynamic sidebar selection for each course

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from 'react';
 import '../../styles/side-bar.css'
 import SideBarItem from './SideBarItem';
-import data from '../../data/sidebar.json';
 
-function SideBar() {
+function SideBar({tutorial}) {
 
-    return (
-        <aside className='sidebar scrollbar'>
-            {data.map((item, index) => (
-                <SideBarItem key={index} item={item} />
-            ))}
-        </aside>
-    )
+  const [sideBarData, setSideBarData] = useState();
+
+  useEffect(() => {
+    async function loadSidebarContent() {
+      try {
+          const module = await import(`../..//data/${tutorial}Sidebar.json`);
+          setSideBarData(module.default);
+      } catch (err) {
+          console.error('Error loading sideBar content:', err);
+      }
+    }
+
+    loadSidebarContent()
+  }, [tutorial])
+
+  if (!sideBarData) return <div>Loading...</div>
+  return (
+    <aside className='sidebar scrollbar'>
+      {sideBarData.map((item, index) => (
+        <SideBarItem key={index} item={item} />
+      ))}
+    </aside>
+  )
 }
 
 export default SideBar;
