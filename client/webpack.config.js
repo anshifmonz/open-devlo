56dd539fb2b9effce3689ceb779c121c53720463
Muhammed Anshif <muhammedanshif236@gmail.com>
2024-09-19 13:53:19 +0530

    refactor(webpack): support production mode, add CleanWebpackPlugin, and improve dynamic port handling

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,62 +1,84 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Portfinder = require('portfinder');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = new Promise((resolve, reject) => {
-  Portfinder.getPortPromise({
-    port: 3000,
-    stopPort: 3333
-  }).then(port => {
-    resolve({
-      mode: 'development',
-      entry: './src/index.js',
-      output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/',
-      },
-      module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  '@babel/preset-env',
-                  ['@babel/preset-react', { runtime: 'automatic' }]
-                ]
-              }
-            }
+module.exports = (_, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? 'bundle.[contenthash].js' : 'bundle.js',
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                ['@babel/preset-react', { runtime: 'automatic' }],
+              ],
+            },
           },
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-        ],
-      },
-      plugins: [
-        new HtmlWebpackPlugin({
-          template: './public/index.html',
-        }),
-      ],
-      stats: 'none',
-      devServer: {
-        static: {
-          directory: path.join(__dirname, 'public'),
         },
-        compress: true,
-        port: port,
-        open: true,
-        hot: true,
-        historyApiFallback: true,
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        minify: isProduction ? {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        } : false,
+      }),
+      isProduction && new CleanWebpackPlugin(),
+    ].filter(Boolean),
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    stats: 'none',
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
       },
-      resolve: {
-        extensions: ['.js', '.jsx'],
-      },
+      compress: true,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+    },
+  };
+
+  if (!isProduction) {
+    return Portfinder.getPortPromise({
+      port: 3000,
+      stopPort: 3333,
+    }).then(port => {
+      config.devServer.port = port;
+      return config;
+    }).catch(err => {
+      throw new Error(`Could not find an available port: ${err}`);
     });
-  }).catch(err => {
-    reject(err);
-  });
-});
+  }
+
+  return config;
+};
