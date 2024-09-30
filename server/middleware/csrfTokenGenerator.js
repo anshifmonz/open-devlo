7800dfd31c169aa5018380ea62f54b1abbca8d71
Muhammed Anshif <muhammedanshif236@gmail.com>
2024-09-30 14:09:58 +0530

    feat(csrf): add expire time for csrf token

--- server/middleware/csrfTokenGenerator.js
+++ server/middleware/csrfTokenGenerator.js
@@ -1,11 +1,30 @@
 import crypto from 'crypto-js';
 
 function csrfTokenGenerator(req, res, next) {
-    if (!req.session.csrfToken) {
-      req.session.csrfToken = crypto.lib.WordArray.random(32).toString();
+  const currentTime = Date.now();
+  const neededExpireTimeLeft = 1000 * 60; // 1 minute
+
+  // if the token not close to expiring then sending the same token back
+  if (req.session.csrfToken) {
+    const timeLeft = req.session.csrfToken.expire - currentTime
+    
+    if (timeLeft > neededExpireTimeLeft) {
+      res.cookie('X-CSRF-TOKEN', req.session.csrfToken.token);
+      return next();
     }
-    res.cookie('X-CSRF-TOKEN', req.session.csrfToken);
-    next();
+  }
+  
+  // If the token doesn't exist or close to expiring
+  delete req.session.csrfToken;
+  res.clearCookie('X-CSRF-TOKEN');
+
+  const csrfToken = crypto.lib.WordArray.random(32).toString();
+  const expireTime = Date.now() + 1000 * 60 * 5; // 5 minute
+
+  req.session.csrfToken = { token: csrfToken, expire: expireTime };
+  res.cookie('X-CSRF-TOKEN', csrfToken);
+
+  next();
 }
 
 export default csrfTokenGenerator;
--- server/middleware/csrfTokenValidator.js
+++ server/middleware/csrfTokenValidator.js
@@ -1,7 +1,14 @@
 function csrfTokenValidator(req, res, next) {
   const csrfTokenFromCookie = req.cookies['X-CSRF-TOKEN'];
-  
-  if (csrfTokenFromCookie && csrfTokenFromCookie === req.session.csrfToken) {
+  const csrfTokenFromSession = req.session?.csrfToken?.token;
+  const expireTime = req.session?.csrfToken?.expire;
+  const currentTime = Date.now();
+
+  if (
+      (csrfTokenFromCookie && csrfTokenFromSession) && 
+      (csrfTokenFromCookie === csrfTokenFromSession) && 
+      (currentTime < expireTime)
+    ) {
     delete req.session.csrfToken;
     res.clearCookie('X-CSRF-TOKEN');
     return next();
